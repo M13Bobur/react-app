da61020bc658896db07156eacc0d74bd5837e7a7
M13Bobur <bobur.mm13@gmail.com>
2023-07-24 22:31:24 +0500

    contracts added

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ const App = () => {
                 <ul>
                     <li><a href="#">Home</a></li>
                     <li><a href="#">About</a></li>
+                    <li><a href="#">Contracts</a></li>
                     <li><a href="#">Contact</a></li>
                 </ul>
             </div>
