781c99d5285a82f6fbb110a1eacd8870dbaf6dd7
M13Bobur <bobur.mm13@gmail.com>
2023-07-24 22:42:40 +0500

    abc

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ const App = () => {
                     <li><a href="#">About</a></li>
                     <li><a href="#">Contracts</a></li>
                     <li><a href="#">Contact</a></li>
+                    <li><a href="#">Sign in</a></li>
                 </ul>
             </div>
             <CustomTable />
