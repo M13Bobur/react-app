68f5e54eb175c654de9216df389aec6ad5db9f03
M13Bobur <bobur.mm13@gmail.com>
2023-07-24 22:25:55 +0500

    Navbar done

--- src/App.js
+++ src/App.js
@@ -8,6 +8,13 @@ const App = () => {
 
     return (
         <div>
+            <div>
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Contact</a></li>
+                </ul>
+            </div>
             <CustomTable />
         </div>
     )
